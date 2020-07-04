fe26f3519c2e60aa3db6b4e8c65a2c155b2b650d
Mikhail Bakalov <mike@mbakalov.com>
2020-07-04 13:43:38 -0400

    Solutions for "Code Forces Global Round 9"

---
+++ Codeforces/GlobalRound9/A/A/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace A
+{
+    /// <summary>
+    /// https://codeforces.com/contest/1375/problem/A
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                int[] a = Console.ReadLine().Split(' ').Select(int.Parse).Select(Math.Abs).ToArray();
+
+                for (int j = 0; j < a.Length; j++)
+                {
+                    if (j % 2 != 0) a[j] = -a[j];
+                }
+
+                Console.WriteLine(string.Join(" ", a.Select(x => x.ToString())));
+            }
+        }
+    }
+}
---
+++ Codeforces/GlobalRound9/B/B/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace B
+{
+    /// <summary>
+    /// https://codeforces.com/contest/1375/problem/B
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.In;
+            var output = Console.Out;
+
+//#if DEBUG
+//            input = new StreamReader("input.txt");
+//            output = new StreamWriter("output.txt");
+//#endif
+
+            int t = int.Parse(input.ReadLine());
+
+            for (int tt = 0; tt < t; tt++)
+            {
+                bool ok = true;
+
+                var ss = input.ReadLine().Split(' ');
+                int n = int.Parse(ss[0]);
+                int m = int.Parse(ss[1]);
+
+                var a = new int[n, m];
+                for (int i = 0; i < n; i++)
+                {
+                    ss = input.ReadLine().Split(' ');
+                    for (int j = 0; j < m; j++)
+                    {
+                        a[i, j] = int.Parse(ss[j]);
+
+                        int target = 0;
+                        if (i + 1 < n) target++;
+                        if (j + 1 < m) target++;
+                        if (i - 1 >= 0) target++;
+                        if (j - 1 >= 0) target++;
+
+                        if (a[i, j] > target)
+                        {
+                            ok = false;
+                            break;
+                        }
+
+                        a[i, j] = target;
+                    }
+                }
+
+                if (ok)
+                {
+                    output.WriteLine("YES");
+                    for (int i = 0; i < n; i++)
+                    {
+                        for (int j = 0; j < m; j++)
+                        {
+                            output.Write(a[i, j]);
+                            if (j < m - 1)
+                            {
+                                output.Write(' ');
+                            }
+                        }
+
+                        output.WriteLine();
+                    }
+                }
+                else
+                {
+                    output.WriteLine("NO");
+                }
+                output.Flush();
+            }
+        }
+    }
+}
